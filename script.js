9b0cdedf1257fd822d6365096964c00ed4775bde
David Wright <davidwright7793@gmail.com>
2024-01-26 19:04:01 -0500

    added variables in JavaScript

---
+++ script.js
@@ -0,0 +1,12 @@
+var time = document.querySelector("#time")
+var startScreen = document.querySelector("#start-screen")
+var startBtn = document.querySelector("#startBtn")
+var questions = document.querySelector("#questions")
+var questionTitle = document.querySelector("#question-title")
+var choices = document.querySelector("#choices")
+var endScreen = document.querySelector("#end-screen")
+var finalScore = document.querySelector("#final-score")
+var initials = document.querySelector("#initials")
+var submit = document.querySelector("#submit")
+
+
